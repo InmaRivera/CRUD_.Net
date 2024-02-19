ceb83c983d7b59551fcc30c1479e7a3b22eb62b7
InmaRivera <conchi.rp94@gmail.com>
2024-02-19 13:14:00 +0100

    new crud in .Net

---
+++ CrudNet7MVC/Models/Contacto.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CrudNet7MVC.Models
+{
+    public class Contacto
+    {
+        [Key]
+        public int Id { get; set; }
+     
+        public string Nombre { get; set; }
+        [Required(ErrorMessage = "El Nombre es obligatorio")]
+
+        public string Telefono { get; set; }
+        [Required(ErrorMessage = "El Telefono es obligatorio")]
+
+        public string Celular { get; set; }
+        [Required(ErrorMessage = "El Celular es obligatorio")]
+
+        public string Email { get; set; }
+        [Required(ErrorMessage = "El Email es obligatorio")]
+        public DateTime FechaCreacion {  get; set; }
+
+    }
+}
